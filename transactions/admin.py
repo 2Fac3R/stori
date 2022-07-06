695eb533aef5e1840ff5555e95bcee44e9378ca9
Gustavo Navarro <genr1818@gmail.com>
2022-07-06 11:45:39 -0500

    Account and Transaction model registration

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,20 @@
+"""Admin Panel Config."""
+
+# Python
+
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# App
+from .models import Account, Transaction
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user')
+    
+        
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'transaction', 'account')
