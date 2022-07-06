ef761ad4c3b93f28db3a170a2a8c437b6651d776
Gustavo Navarro <genr1818@gmail.com>
2022-07-06 12:01:14 -0500

    Urls pointing to views

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,7 +4,17 @@
 from django.urls import path
 
 # App
+from .views import TransactionView as transaction_views
+from .views import AccountView as account_views
+
 
 urlpatterns = [
     # Transactions
+    path('transactions/', transaction_views.TransactionListView.as_view(), name='transaction'),
+    path('transactions/<int:pk>', transaction_views.TransactionDetailView.as_view(),
+         name='transaction-detail'),
+    # Accounts
+    path('accounts/', account_views.AccountListView.as_view(), name='account'),
+    path('accounts/<int:pk>', account_views.AccountDetailView.as_view(),
+         name='account-detail'),
 ]
