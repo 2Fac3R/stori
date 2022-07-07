2e3ed9fdcdc34fc7fe0a5a5eabf4cfb802babd80
Gustavo Navarro <genr1818@gmail.com>
2022-07-07 13:41:14 -0500

    Fix typo: path list  name must be in plural

--- transactions/urls.py
+++ transactions/urls.py
@@ -11,11 +11,11 @@ from .views import AccountView as account_views
 urlpatterns = [
     # Transactions
     path('transactions/', transaction_views.TransactionListView.as_view(),
-         name='transaction'),
+         name='transactions'),
     path('transactions/<int:pk>', transaction_views.TransactionDetailView.as_view(),
          name='transaction-detail'),
     # Accounts
-    path('accounts/', account_views.AccountListView.as_view(), name='account'),
+    path('accounts/', account_views.AccountListView.as_view(), name='accounts'),
     path('accounts/<int:pk>', account_views.AccountDetailView.as_view(),
          name='account-detail'),
 ]
