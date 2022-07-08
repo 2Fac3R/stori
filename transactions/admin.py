a98d8a76bc3d5bd63b975242abfb1f4ae56d0bdd
Gustavo Navarro <genr1818@gmail.com>
2022-07-08 12:44:28 -0500

    Email feature added to admin site

--- transactions/admin.py
+++ transactions/admin.py
@@ -2,12 +2,17 @@
 
 # Python
 import datetime
+from operator import countOf
 
 # Django
 from django.contrib import admin, messages
 from django.urls import path, reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.template.loader import render_to_string
 
 # App
 from .models import Account, Transaction
@@ -18,6 +23,58 @@ from .forms import CsvImportForm
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('id', 'user')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('send-email/', self.send_email)]
+        return new_urls + urls
+
+    def send_email(self, request):
+        account = Account.objects.filter(user=request.user)
+        operations = Transaction.objects.filter(
+            account=account[0:1]).values_list('transaction', flat=True)
+        months = Transaction.objects.filter(
+            account=account[0:1]).values_list('date__month', flat=True)
+
+        months_of_year = [
+            'January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December'
+        ]
+        number_month_tuple = [(x, m)
+                              for x, m in zip(range(1, 13), months_of_year)]
+        credit = sum([float(x) for x in operations if float(x) > 0] or None)
+        debit = sum([float(x) for x in operations if float(x) < 0] or None)
+        credit_operations = [float(x)
+                             for x in operations if float(x) > 0] or None
+        debit_operations = [float(x)
+                            for x in operations if float(x) < 0] or None
+        total_balance = sum([float(x) for x in operations] or None)
+        average_credit = credit/len(credit_operations)
+        average_debit = debit/len(debit_operations)
+        number_transactions_each_month = [[number_month_tuple[x-1], countOf(months, x)]
+                                          for x in range(1, 13)] or None
+
+        msg_html = render_to_string(
+            'email/account_info.html',
+            {
+                'account': account.first,
+                'total_balance': total_balance,
+                'average_credit': average_credit,
+                'average_debit': average_debit,
+                'number_transactions_each_month': number_transactions_each_month
+            })
+        if request.method == "GET":
+            try:
+                send_mail(
+                    'Stori: Account summary info', 'Account info',
+                    settings.EMAIL_HOST_USER,
+                    [request.user.email],
+                    html_message=msg_html)
+                self.message_user(request, "Email succesfully sent!")
+            except Exception as error:
+                messages.warning(request, error)
+            url = reverse('admin:transactions_account_changelist')
+            return HttpResponseRedirect(url)
 
 
 @admin.register(Transaction)
@@ -59,6 +116,7 @@ class TransactionAdmin(admin.ModelAdmin):
                 except ValueError as error:
                     messages.warning(request, error)
                     return HttpResponseRedirect(request.path_info)
+            self.message_user(request, "File succesfully uploaded!")
             url = reverse('admin:transactions_transaction_changelist')
             return HttpResponseRedirect(url)
 
